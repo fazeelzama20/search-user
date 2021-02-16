2f2fd2757d883f7a9d95478e37c284b8e7394d90
Fazeel Zama <fazeel.zama20@gmail.com>
2021-02-16 16:50:09 +0100

    Update SearchUser.tsx

--- src/SearchUser.tsx
+++ src/SearchUser.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 export const SearchUser: React.FunctionComponent = () => {
 
     const userList = [
@@ -9,9 +9,21 @@ export const SearchUser: React.FunctionComponent = () => {
         { id: 5, name: 'delhi', age: 60 }
     ];
 
+    // we need to provide the type of html element which we are going to refer
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [name, setName] = useState('');
     const [user, setUser] = useState<{ id: number, name: string, age: number | undefined }>();
     const [isFound, setFound] = useState('');
+
+    // if you add 2nd arg as empty array then inner function will execute once
+    useEffect(() => {
+        /* we need to check if the refered element should be present, because null value is also in a type.
+        so focus function cannot be applied to null element */
+        if (!inputRef.current) {
+            return;
+        }
+        inputRef.current.focus();
+    }, [])
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         setName(event?.target.value);
@@ -33,7 +45,7 @@ export const SearchUser: React.FunctionComponent = () => {
             {userList.map((item, index) => {
                 return <div key={index}>{index + 1}. {item.name} {item.age}</div>
             })}
-            <input value={name} onChange={handleInput} />
+            <input ref={inputRef} value={name} onChange={handleInput} />
             <button onClick={findUser}>Find User</button>
             <div>{user && 'Name: ' + user.name}</div>
             <div>{user && 'Age: ' + user.age}</div>
